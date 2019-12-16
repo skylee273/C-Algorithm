d9e001b06d05607dad9fef79ab910a618ba8bb88
이하늘 <skylee273@koreatech.ac.kr>
2019-12-16 18:19:13 +0900

    2019-12-16 linear time Algorithm

--- Algorithm_Project_MovingAverage/main.cpp
+++ Algorithm_Project_MovingAverage/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <vector>
+#include <ctime>
 
 using namespace std;
 
@@ -18,11 +19,27 @@ vector<double> movingAverage1(const vector<double>& A, int M) {
 	}
 	return ret;
 }
-
+vector<double> movingAverage2(const vector<double>& A, int M) {
+	int N = A.size();
+	double partialSum = 0.0;
+	vector <double> ret;
+	for (int i = 0; i < M - 1; ++i) {
+		partialSum += A[i];
+	}
+	for (int i = M - 1; i < N; ++i) {
+		partialSum += A[i];
+		ret.push_back(partialSum / M);
+		partialSum -= A[i - M + 1];
+	}
+	return ret;
+}
 int main() {
 	vector<double> sample;
 	vector<double> answer;
-
+	vector<double> answer2;
+	clock_t start, end;
+	double result;
+	int startTime = 0;
 	sample.push_back(50.3);
 	sample.push_back(50.7);
 	sample.push_back(50.1);
@@ -32,10 +49,21 @@ int main() {
 	sample.push_back(47.4);
 
 	answer = movingAverage1(sample, 2);
+	answer2 = movingAverage2(sample, 2);
 
 	int size = answer.size();
+	start = clock();
 	for (int i = 0; i < size; ++i) {
 		printf("%3.5lf\n", answer[i]);
 	}
+	end = clock();
+	printf("측정 시간 = %f\n", (double)(end - start));
+	start = clock();
+	for (int i = 0; i < size; ++i) {
+		printf("%3.5lf\n", answer[i]);
+	}
+	end = clock();
+	printf("측정 시간 = %f\n", (double)(end - start));
+
 	return 0;
 }
