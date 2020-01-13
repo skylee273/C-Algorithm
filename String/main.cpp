0fe61bc83eaa19e5831c4332dafb53d564fbab9a
이하늘 <skylee273@koreatech.ac.kr>
2020-01-13 14:00:42 +0900

    2020-01-13 String(BruteForce, Kmp)

--- String/main.cpp
+++ String/main.cpp
@@ -17,10 +17,47 @@ bool bruteForce(string s, string match) {
 	return false;
 }
 
+vector<int> getPi(string p) {
+	int m = p.size();
+	vector<int> pi(m, 0);
+	int j = 0;
+	for (int i = 1; i < m; ++i) {
+		while (j > 0 && p[i] != p[j]) {
+			j = pi[j - 1];
+		}
+		if (p[i] == p[j]) {
+			pi[i] = ++j;
+		}
+	}
+	return pi;
+}
+bool kmp(string s, string p) {
+	int n = s.size();
+	int m = p.size();
+	auto pi = getPi(p);
+	int j = 0;
+	for (int i = 0; i < n; ++i) {
+		if (j > 0 && s[i] != p[j]) {
+			j = pi[j - 1];
+		}
+		if (s[i] == p[j]) {
+			if (j == m - 1)
+				return true;
+			else
+				++j;
+		}
+	}
+	return false;
+}
+
 int main() {
-	string s = "a pattern matching algorithm";
-	string match = "rithm";
+	string s = "ABABABABBABABABABC";
+	string match = "ABABABABC";
 	bool check = bruteForce(s, match);
 	if (check)
 		printf("ok\n");
+	bool check2 = kmp(s, match);
+	if (check2) {
+		printf("ok\n");
+	}
 }
