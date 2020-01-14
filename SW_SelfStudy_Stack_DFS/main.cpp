953f5a9e254e9f9faa040039668c0d5c59ddf4c5
이하늘 <skylee273@koreatech.ac.kr>
2020-01-14 21:59:10 +0900

    2020-01-14 21:58 SW_SelfStudy_Stack_DFS

---
+++ SW_SelfStudy_Stack_DFS/main.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+vector<int> st;
+vector <vector<int>> arr;
+
+vector <bool> visit(8, false);
+void dfs(int v) {
+	
+	st.push_back(v);
+	printf("%d ", v);
+	visit[v] = true;
+	while (!st.empty()) {
+		for (int i = 1; i <= 7; ++i) {
+			if (visit[i] == false && arr[st.back()][i] == 1) {
+				st.push_back(i);
+				printf("%d ", i);
+				visit[i] = true;
+				i = 1;
+			}
+		}
+		st.pop_back();
+	}
+}
+int main() {
+	arr.assign(8, vector<int>(8, 0));
+	int v1, v2;
+	for (int i = 0; i < 7; ++i) {
+		scanf("%d %d", &v1, &v2);
+		arr[v1][v2] = 1;
+		arr[v2][v1] = 1;
+	}
+	dfs(1);
+	arr.clear();
+	return 0;
+}
