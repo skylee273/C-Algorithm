816804bf436ba23b3638353ea53e3dfcc30b2ed7
이하늘 <skylee273@koreatech.ac.kr>
2020-02-06 16:33:46 +0900

    2020-02-05 16:33 Sw1245 D5 (Binary Search)

--- SW1245/main.cpp
+++ SW1245/main.cpp
@@ -1,10 +1,12 @@
 #include <cstdio>
 #include <vector>
-
+#include <cmath>
+#include <algorithm>
+#include <cstring>
 using namespace std;
 
-int x[10];
-int m[10];
+double x[10];
+double m[10];
 int n;
 const double Eps = 0.000000000001;
 
@@ -15,10 +17,11 @@ void findX() {
 		double right = x[i+1];
 		double mid;
 		double sum;
+		double ans;
 		for (int k = 0; k < 100; ++k) {
 			// 왼쪽 합
 			mid = (left + right) / 2.0;
-			sum = 0.0;
+			sum = 0, ans = 0;
 			for (int j = 0; j <= i; ++j) {
 				sum += (m[j]) / ((mid - x[j]) * (mid - x[j]));
 			}
@@ -27,17 +30,21 @@ void findX() {
 				sum -= (m[j]) / ((x[j] - mid) * (x[j] - mid));
 			}
 			
-			// 만약 0 이 되었을떄
-			if (abs(sum) < Eps) {
-				answer.push_back(mid);
-				break;
-			}else if (sum > 0) {
+			//// 만약 0 이 되었을떄
+			//if (abs(sum) < Eps) {
+			//	
+			//	break;
+			//}
+			//
+			if (sum > 0) {
 				left = mid;
 			}
 			else {
+				ans = mid;
 				right = mid;
 			}
 		}
+		answer.push_back(mid);
 	}
 }
 int main() {
@@ -49,10 +56,10 @@ int main() {
 		memset(x, 0, sizeof(x));
 		memset(m, 0, sizeof(m));
 		for (int i = 0; i < n; ++i) {
-			scanf(" %d", &x[i]);
+			scanf(" %lf", &x[i]);
 		}
 		for (int i = 0; i < n; ++i) {
-			scanf(" %d", &m[i]);
+			scanf(" %lf", &m[i]);
 		}
 		findX();
 		printf("#%d ", tc);
